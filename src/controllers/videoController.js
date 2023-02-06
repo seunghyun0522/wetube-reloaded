61f0b588d99e0ee9ab0d1bc54bf631b5de7a3ce5
seunghyun <cvtcvt007@naver.com>
2023-02-06 14:57:42 +0900

    #5.3 inheritant extends & block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,11 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("delete Video");
 };
 
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit Video");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send("see videos");
-};
+
 export const upload = (req, res) => res.send("upload videos");
--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
+console.log(process.cwd());
 const app = express();
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
