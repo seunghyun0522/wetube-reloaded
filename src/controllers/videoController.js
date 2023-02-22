617cc6151adf7f56bf67352751c6b0680fd6c687
seunghyun <cvtcvt007@naver.com>
2023-02-22 16:51:47 +0900

    #6.28 search... delete file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -57,4 +57,26 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  //delete Video
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  // console.log(req.query); keyword 의 값을 볼 수 있다.
+  console.log("should search for ", keyword);
+  if (keyword) {
+    //search!!
+    videos = await Video.find({
+      title: { $regex: new RegExp(`^${keyword}`, "i") },
+    });
+    console.log(videos);
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
