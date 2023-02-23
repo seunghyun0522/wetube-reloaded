dc6cf81edd07e28221d438f372405f91d5ac8bec
seunghyun <cvtcvt007@naver.com>
2023-02-23 16:18:28 +0900

    #7.6 login

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
 import Video from "../models/Video";
-
 export const home = async (req, res) => {
   const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
-
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -13,22 +11,20 @@ export const watch = async (req, res) => {
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
-
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
-
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -37,11 +33,9 @@ export const postEdit = async (req, res) => {
   });
   return res.redirect(`/videos/${id}`);
 };
-
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
@@ -52,31 +46,26 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
   }
 };
-
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
-  //delete Video
   return res.redirect("/");
 };
-
 export const search = async (req, res) => {
   const { keyword } = req.query;
   let videos = [];
-  // console.log(req.query); keyword 의 값을 볼 수 있다.
-  console.log("should search for ", keyword);
   if (keyword) {
-    //search!!
     videos = await Video.find({
-      title: { $regex: new RegExp(`^${keyword}`, "i") },
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
     });
-    console.log(videos);
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
