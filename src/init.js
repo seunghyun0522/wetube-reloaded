25127e2e9687723c61217dc5b9a03f6818a2eddb
seunghyun <cvtcvt007@naver.com>
2023-03-11 21:47:18 +0900

    init extra commit

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/Video";
@@ -8,6 +9,6 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
