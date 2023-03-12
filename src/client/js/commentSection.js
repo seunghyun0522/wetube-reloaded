b3c2464f208123f5b6a73105f99c701db1577f8e
seunghyun <cvtcvt007@naver.com>
2023-03-12 21:30:11 +0900

    last video

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,8 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+let deleteComments = document.querySelectorAll("#delete__comment");
+
 const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
@@ -12,6 +14,7 @@ const addComment = (text, id) => {
   span.innerText = ` ${text}`;
   const span2 = document.createElement("span");
   span2.innerText = "❌";
+  span2.id = "delete__comment";
   newComment.appendChild(icon);
   newComment.appendChild(span);
   newComment.appendChild(span2);
@@ -19,7 +22,7 @@ const addComment = (text, id) => {
 };
 
 const handleSubmit = async (event) => {
-  event.preventDefault();
+  event.preventDefault(); // 새로고침 방지
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
@@ -28,18 +31,39 @@ const handleSubmit = async (event) => {
   }
   const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-type": "application/json" }, // information about request
     body: JSON.stringify({ text }),
   });
+
   if (response.status === 201) {
     textarea.value = "";
-    const { newCommentId } = await response.json();
-    addComment(text, newCommentId);
+    const json = await response.json(); // json 추출.
+    addComment(text, json.newCommentId);
+    deleteComment = document.getElementById("delete__comment");
+    deleteComment.removeEventListener("click", handleDeleteComment);
+    deleteComment.addEventListener("click", handleDeleteComment);
   }
+  // window.location.reload();
 };
 
 if (form) {
   form.addEventListener("submit", handleSubmit);
+}
+
+const handleDeleteComment = async (event) => {
+  const li = event.srcElement.parentNode;
+  const {
+    dataset: { id: commentId },
+  } = li;
+
+  await fetch(`/api/comments/${commentId}/delete`, {
+    method: "DELETE",
+  });
+  li.remove();
+};
+
+if (deleteComments) {
+  deleteComments.forEach((deleteComment) => {
+    deleteComment.addEventListener("click", handleDeleteComment);
+  });
 }
